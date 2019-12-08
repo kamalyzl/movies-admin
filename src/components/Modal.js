361fac851ba0a366ddb6a3f25e05ee44d4dad75c
zlkamaly <kamaly@krealo.pe>
2019-12-08 11:28:26 -0500

    prepared deploy

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,10 +3,6 @@ import React from 'react'
 const Modals = ({ show, onHide }) => {
   return (
     <>
-      {/* <Button variant='primary' onClick={handleShow}>
-        Launch demo modal
-      </Button> */}
-
       <Modal show={show} onHide={onHide}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
