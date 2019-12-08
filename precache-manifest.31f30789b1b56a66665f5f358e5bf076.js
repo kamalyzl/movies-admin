165f523dc29068f2b0674522e050af4e76fbc5e0
zlkamaly <kamaly@krealo.pe>
2019-12-08 00:12:28 -0500

    Updates

---
+++ precache-manifest.31f30789b1b56a66665f5f358e5bf076.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a8e576ee4044897721a64bc9a6c55c",
+    "url": "/movies-admin/index.html"
+  },
+  {
+    "revision": "299b07b6d5cf67fa536b",
+    "url": "/movies-admin/static/css/main.4af9d244.chunk.css"
+  },
+  {
+    "revision": "95c15928a2165588c4ba",
+    "url": "/movies-admin/static/js/2.bac7a550.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movies-admin/static/js/2.bac7a550.chunk.js.LICENSE"
+  },
+  {
+    "revision": "299b07b6d5cf67fa536b",
+    "url": "/movies-admin/static/js/main.91d73a68.chunk.js"
+  },
+  {
+    "revision": "97053f2b9e4690099560",
+    "url": "/movies-admin/static/js/runtime-main.d5fe4771.js"
+  }
+]);
